d0d33def0e81e93c862fa895199629af9682dc16
Asikahamed <96490250+Asikahamed@users.noreply.github.com>
2026-01-26 22:13:25 +0530

    Refactor service account IAM member resource

--- Service-account/main.tf
+++ Service-account/main.tf
@@ -1,15 +1,23 @@
 resource "google_service_account" "this" {
-  for_each     = var.service_accounts
+  for_each = var.service_accounts
+
   project      = var.project_id
   account_id   = each.value.account_id
   display_name = each.value.display_name
-  description  = each.value.description 
+  description  = each.value.description
 }
 
 resource "google_project_iam_member" "sa_roles" {
-  for_each = toset(var.roles)
-  project = var.project_id
-  role    = each.value
-  member  = "serviceAccount:${google_service_account.this.email}"
-}
+  for_each = {
+    for sa_key, sa in google_service_account.this :
+    for role in var.roles :
+    "${sa_key}-${role}" => {
+      email = sa.email
+      role  = role
+    }
+  }
 
+  project = var.project_id
+  role    = each.value.role
+  member  = "serviceAccount:${each.value.email}"
+}
