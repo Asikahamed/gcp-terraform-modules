173bc1cde2f81117f0f31b537ca814cba8820c48
Asikahamed <96490250+Asikahamed@users.noreply.github.com>
2026-01-26 20:48:22 +0530

    Fix service account member reference in IAM roles

--- Service-account/main.tf
+++ Service-account/main.tf
@@ -10,5 +10,5 @@ resource "google_project_iam_member" "sa_roles" {
   for_each     = toset(var.roles)
   project      = var.project_id
   role         = each.value
-  member       = "serviceAccount:${google_service_account.this[each.key].email}"
+  member        = "serviceAccount:${google_service_account.this.email}"
 }
